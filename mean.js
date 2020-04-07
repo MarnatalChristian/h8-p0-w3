946a1553c8e8c4d0c7f04593487f818ee837a12f
ChristianPardede <christian.kodz@gmail.com>
2020-04-07 22:34:27 +0700

    update source code yang  kurang

--- mean.js
+++ mean.js
@@ -16,4 +16,4 @@ console.log(cariMean([1, 2, 3, 4, 5])); // 3
 console.log(cariMean([3, 5, 7, 5, 3])); // 5
 console.log(cariMean([6, 5, 4, 7, 3])); // 5
 console.log(cariMean([1, 3, 3])); // 2
-console.log(cariMean([8, 8, 8, 8, 8])); // 
+console.log(cariMean([8, 8, 8, 8, 8])); // 8
